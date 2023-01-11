0f0d35e9b3e258066c5a2bd34ab141704be82e20
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-01-11 17:07:29 -0500

    modified the configuration for extension

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+/*global chrome*/
 import './App.css';
 
 function App() {
+  const handleClickGetText = () => {
+    chrome.tabs.executeScript(null, {
+      code: 'console.log(document.documentElement.innerText);',
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClickGetText}>Get Text</button>
     </div>
   );
 }
