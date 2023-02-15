c7b1f7e31b72c1c0b351e7f3c8a232c705268702
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-02-15 11:04:39 -0500

    removed selecting field

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -15,33 +15,6 @@ const Card = ({setExtractField}) => {
 
   return(
     <div class="main-container">
-      <InputLabel size="normal" focused>Please select what kind of lists you want to extract. </InputLabel>
-      <div class="cards">
-        <div class="card card-1" onClick={() => {
-          setChecked('company')
-          setExtractField('company')
-          setOther('company')
-        }}>
-          <div class="card__icon"><Checkbox {...label} defaultChecked checked={checked=='company'?true:false} style = {{marginTop:'-68px', marginLeft: '-18px'}}/></div>
-          <p class="card__exit"><i class="fas fa-times"></i></p>
-          <h2 class="card__title">Company</h2>
-          <p class="card__apply">
-            <a class="card__link" href="#">Extract name of the company from the text <i class="fas fa-arrow-right"></i></a>
-          </p>
-        </div>
-        <div class="card card-3" onClick={() => {
-          setChecked('software')
-          setExtractField('software')
-          setOther('software')
-        }}>
-          <div class="card__icon"><Checkbox {...label} defaultChecked checked={checked=='software'?true:false} color="secondary" style = {{marginTop:'-68px', marginLeft: '-18px'}}/></div>
-          <p class="card__exit"><i class="fas fa-times"></i></p>
-          <h2 class="card__title">Software</h2>
-          <p class="card__apply">
-            <a class="card__link" href="#">Extract name of the software from the text <i class="fas fa-arrow-right"></i></a>
-          </p>
-        </div>
-      </div>
       <br></br>
       <InputLabel size="normal" focused>Please type what kind of lists you want to extract. </InputLabel>
       <Box
@@ -55,6 +28,7 @@ const Card = ({setExtractField}) => {
       <TextField
         id="standard-basic" 
         value={other}
+        placeholder ="company"
         onChange={(event) => {
           setOther(event.target.value);
           setChecked(event.target.value);
