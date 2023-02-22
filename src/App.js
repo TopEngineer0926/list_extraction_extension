f4047dc3860f5d3bf9f118efc7512d7fa1801dec
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-02-22 15:31:37 -0500

    Modified endpoint.

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ import TextCaptureButton from './components/TextCaptureButton';
 import TitlePanel from './components/TitlePanel';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-// const API_ENDPOINT = 'https://moonhub-list-backend.herokuapp.com/api';
-const API_ENDPOINT = 'https://moonhub-list-backend-develop.herokuapp.com/api';
+const API_ENDPOINT = 'https://moonhub-list-backend.herokuapp.com/api';
 
 // const API_ENDPOINT = 'http://localhost:8000/api';
 
