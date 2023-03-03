410919e9c455425abafe10ec7e0e84a11b44732c
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-03-03 10:27:40 -0500

    wrote the code to reset the window size when resizing is detected

--- public/background.js
+++ public/background.js
@@ -1,31 +1,43 @@
 /*global chrome*/
 var currentTab;
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Chrome extension successfully installed!');
+  console.log("Chrome extension successfully installed!");
   return;
 });
+
+const DEFAULT_WIDTH = 660;
+const DEFAULT_HEIGHT = 630;
 
 chrome.action.onClicked.addListener(createPanel);
 function createPanel(tab) {
   currentTab = tab;
   if (!tab) return;
-  const contentWindowId = tab.windowId;
   try {
-    const panelWindowInfo = chrome.windows.create({
-      url: chrome.runtime.getURL('index.html'),
-      type: 'panel',
-      height: 630,
-      width: 660
+    chrome.windows.create({
+      url: chrome.runtime.getURL("index.html"),
+      type: "popup",
+      top: 200,
+      left: 400,
+      height: DEFAULT_HEIGHT,
+      width: DEFAULT_WIDTH,
     });
   } catch (error) {
     console.log(error);
   }
 }
 
+chrome.windows.onBoundsChanged.addListener(function (windowId) {
+  // Do something when the window is resized
+  chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT, {
+    height: DEFAULT_HEIGHT,
+    width: DEFAULT_WIDTH,
+  });
+});
+
 chrome.runtime.onConnect.addListener(function (port) {
-  console.assert(port.name === 'init_list_extraction');
+  console.assert(port.name === "init_list_extraction");
   port.onMessage.addListener(function (msg) {
-    if (msg.type === 'get-user-data') {
+    if (msg.type === "get-user-data") {
       function modifyDOM() {
         //You can play with your DOM here or check URL against your regex
         return document.documentElement.innerText;
