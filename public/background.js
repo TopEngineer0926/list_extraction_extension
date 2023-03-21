ef8c45876d46539ad86428fde8be7ac659d33fd6
TopEngineer0926 <top_engineer_0926@outlook.com>
2023-03-21 12:34:11 -0400

    fixed the issue that the browser is resized multiple times even the extension is closed

--- public/background.js
+++ public/background.js
@@ -10,6 +10,7 @@ const DEFAULT_HEIGHT = 750;
 const DEFAULT_TOP = 200;
 const DEFAULT_LEFT = 400;
 
+var extensionWindow = null;
 chrome.action.onClicked.addListener(createPanel);
 function createPanel(tab) {
   currentTab = tab;
@@ -17,14 +18,19 @@ function createPanel(tab) {
   try {
     chrome.windows.getAll({ windowTypes: ["popup"] }, function (windows) {
       if (windows.length === 0) {
-        chrome.windows.create({
-          url: chrome.runtime.getURL("index.html"),
-          type: "popup",
-          top: DEFAULT_TOP,
-          left: DEFAULT_LEFT,
-          height: DEFAULT_HEIGHT,
-          width: DEFAULT_WIDTH,
-        });
+        chrome.windows.create(
+          {
+            url: chrome.runtime.getURL("index.html"),
+            type: "popup",
+            top: DEFAULT_TOP,
+            left: DEFAULT_LEFT,
+            height: DEFAULT_HEIGHT,
+            width: DEFAULT_WIDTH,
+          },
+          function (w) {
+            extensionWindow = w;
+          }
+        );
       }
     });
   } catch (error) {
@@ -34,10 +40,12 @@ function createPanel(tab) {
 
 chrome.windows.onBoundsChanged.addListener(function (windowId) {
   // Do something when the window is resized
-  chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT, {
-    height: DEFAULT_HEIGHT,
-    width: DEFAULT_WIDTH,
-  });
+  if (extensionWindow) {
+    chrome.windows.update(extensionWindow.id, {
+      height: DEFAULT_HEIGHT,
+      width: DEFAULT_WIDTH,
+    });
+  }
 });
 
 chrome.runtime.onConnect.addListener(function (port) {
